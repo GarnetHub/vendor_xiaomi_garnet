aeb249f04a3b11ee2f0ca483b08fe20bd097a4b3
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-09-16 07:04:41 +0300

    Update garnet-vendor.mk

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -949,9 +949,6 @@ PRODUCT_PACKAGES += \
     libllvm-qcom \
     libllvm-qgl \
     libvmmem \
-    libqcbassboost \
-    libqcreverb \
-    libqcvirt \
     vendor.qti.qspmhal@1.0-impl \
     vendor.qti.qspmhal@1.0 \
     android.hardware.secure_element@1.0-impl \
